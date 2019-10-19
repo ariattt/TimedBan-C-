2c18dd705b3f58fd9cd301dbdd026806a155b7f5
Bazinga <Bazinga@Bazingas-MBP.attlocal.net>
2019-10-19 17:30:13 -0500

    done formatting; next timed locking the file

--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 args = `arg="$(filter-out $@,$(MAKECMDGOALS))" && echo $${arg:-${1}}`
 
 all:
-	g++ pro.cc -o ha
+	g++ -std=c++14 pro.cc -o ha
 	./ha
 
 ban:
-	g++ pro.cc -o ha
+	g++ -std=c++14 pro.cc -o ha
 	./ha $(call args)
 
 clear:
--- pro.cc
+++ pro.cc
@@ -42,6 +42,7 @@ void remove_duplicate(vector<string> cur, vector<string>& tar){
         vector<string> res;
         boost::split(res, cur[i], boost::is_any_of(" "));// [](char c){return c==' ';});
         if(res.size() == 2){
+//            string ip_pattern = R"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
             std::regex ip ("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
             std::regex web (".*\..*");
             if(std::regex_match(res[0], ip) && std::regex_match(res[1], web))
@@ -58,6 +59,23 @@ void remove_duplicate(vector<string> cur, vector<string>& tar){
     }
 }
 
+vector<string> update_content(const vector<string> content, const vector<string> targets){
+    vector<string> new_content (content);
+    
+    new_content.push_back("\n# Added By TimedBan");
+    new_content.push_back("# --------------------------------------");
+    
+    for(string s : targets){
+        new_content.push_back("127.0.0.1 " + s);
+    }
+    
+    new_content.push_back("# --------------------------------------");
+    new_content.push_back("# End of TimedBan Section\n");
+
+    return new_content;
+    
+}
+
 int main(int argc, char** argv){
 
     vector<string> target_sites = clean_sites(argc, argv);
@@ -70,19 +88,25 @@ int main(int argc, char** argv){
     ifstream cur_host;
     cur_host.open("/etc/hosts");
 
-    vector<string> file;
+    vector<string> content;
     string line;
     while(getline(cur_host, line)){
-        file.push_back(line);
+        content.push_back(line);
     }
 
-    remove_duplicate(file, target_sites);
+    remove_duplicate(content, target_sites);
 
-    cout<<"current host file: \n";
-    for(string s : file)
+    vector<string> new_content = update_content(content, target_sites);
+
+    cout<<"*** current host file: \n";
+    for(string s : content)
+        cout<<s<<endl;
+    
+    cout<<"*** target host file: \n";
+    for(string s : new_content)
         cout<<s<<endl;
 
-    cout<<"target sites: \n";
+    cout<<"*** target sites: \n";
     for(string s : target_sites)
         cout<<s<<endl;
 
